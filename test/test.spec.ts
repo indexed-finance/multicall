b9f149a3b394be2272e3c37d5729630efef9ad80
dillon <dillonkellar@protonmail.com>
2020-10-31 20:06:10 -0500

    Added tests for other input options

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { BigNumber, Contract } from 'ethers';
 import { ethers } from 'hardhat';
 import { CallInput, MultiCall } from '../src';
-
+const { abi } = require('../artifacts/contracts/test/TestContract.sol/TestContract.json');
 
 describe('MultiCall', async () => {
   let testContract: Contract;
@@ -13,7 +13,7 @@ describe('MultiCall', async () => {
     multiCall = new MultiCall(ethers.provider);
   });
 
-  it('gets data from test contract', async () => {
+  it('inputs: CallInput[]', async () => {
     const baseInput: CallInput = {
       interface: testContract.interface,
       args: [],
@@ -38,5 +38,57 @@ describe('MultiCall', async () => {
     ]);
     expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
     expect(testStruct.b).to.deep.eq([50, 2]);
-  })
+  });
+
+  it('inputs: Interface, CallInput[]', async () => {
+    const baseInput: CallInput = {
+      args: [],
+      target: testContract.address,
+      function: ''
+    };
+    const inputs = [
+      { ...baseInput, function: 'getUint' },
+      { ...baseInput, function: 'getUintArray' },
+      { ...baseInput, function: 'getStruct' }
+    ];
+    const result = await multiCall.multiCall(testContract.interface, inputs);
+    const [
+      testuint,
+      testarray,
+      testStruct
+    ] = result;
+    expect(testuint).to.eq(BigNumber.from(10).pow(20));
+    expect(testarray).to.deep.eq([
+      BigNumber.from(10).pow(20),
+      BigNumber.from(10).pow(18)
+    ]);
+    expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
+    expect(testStruct.b).to.deep.eq([50, 2]);
+  });
+
+  it('inputs: JsonFragment[], CallInput[]', async () => {
+    const baseInput: CallInput = {
+      args: [],
+      target: testContract.address,
+      function: ''
+    };
+    const inputs = [
+      { ...baseInput, function: 'getUint' },
+      { ...baseInput, function: 'getUintArray' },
+      { ...baseInput, function: 'getStruct' }
+    ];
+    const result = await multiCall.multiCall(abi, inputs);
+    const [
+      testuint,
+      testarray,
+      testStruct
+    ] = result;
+    expect(testuint).to.eq(BigNumber.from(10).pow(20));
+    expect(testarray).to.deep.eq([
+      BigNumber.from(10).pow(20),
+      BigNumber.from(10).pow(18)
+    ]);
+    expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
+    expect(testStruct.b).to.deep.eq([50, 2]);
+  });
 });
