c21b58a4860b7a935002d76333110849ee23da88
dillon <dillonkellar@protonmail.com>
2020-11-05 14:42:35 -0600

    added tests for fns with multiple return params

--- test/MultiCall.spec.ts
+++ test/MultiCall.spec.ts
@@ -21,21 +21,32 @@ describe('MultiCall', async () => {
     };
     const inputs = [
       { ...baseInput, function: 'getUint' },
+      { ...baseInput, function: 'getUint2' },
       { ...baseInput, function: 'getUintArray' },
+      { ...baseInput, function: 'getUintArray2' },
       { ...baseInput, function: 'getStruct' },
       { ...baseInput, function: 'doRevert' }
     ];
     const result = await multiCall.multiCall(inputs);
     const [
       testuint,
+      testuint2,
       testarray,
+      testarray2,
       testStruct,
       testRevert
     ] = result;
     expect(testuint).to.eq(BigNumber.from(10).pow(20));
+    expect(testuint2).to.deep.eq([
+      BigNumber.from(10).pow(18),
+      BigNumber.from(10).pow(19)
+    ])
     expect(testarray).to.deep.eq([
       BigNumber.from(10).pow(20),
       BigNumber.from(10).pow(18)
+    ]);
+    expect(testarray2).to.deep.eq([
+      BigNumber.from(10).pow(20)
     ]);
     expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
     expect(testStruct.b).to.deep.eq([50, 2]);
@@ -60,21 +71,32 @@ describe('MultiCall', async () => {
     };
     const inputs = [
       { ...baseInput, function: 'getUint' },
+      { ...baseInput, function: 'getUint2' },
       { ...baseInput, function: 'getUintArray' },
+      { ...baseInput, function: 'getUintArray2' },
       { ...baseInput, function: 'getStruct' },
       { ...baseInput, function: 'doRevert' }
     ];
     const result = await multiCall.multiCall(testContract.interface, inputs);
     const [
       testuint,
+      testuint2,
       testarray,
+      testarray2,
       testStruct,
       testRevert
     ] = result;
     expect(testuint).to.eq(BigNumber.from(10).pow(20));
+    expect(testuint2).to.deep.eq([
+      BigNumber.from(10).pow(18),
+      BigNumber.from(10).pow(19)
+    ])
     expect(testarray).to.deep.eq([
       BigNumber.from(10).pow(20),
       BigNumber.from(10).pow(18)
+    ]);
+    expect(testarray2).to.deep.eq([
+      BigNumber.from(10).pow(20)
     ]);
     expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
     expect(testStruct.b).to.deep.eq([50, 2]);
@@ -98,21 +120,32 @@ describe('MultiCall', async () => {
     };
     const inputs = [
       { ...baseInput, function: 'getUint' },
+      { ...baseInput, function: 'getUint2' },
       { ...baseInput, function: 'getUintArray' },
+      { ...baseInput, function: 'getUintArray2' },
       { ...baseInput, function: 'getStruct' },
       { ...baseInput, function: 'doRevert' }
     ];
     const result = await multiCall.multiCall(abi, inputs);
     const [
       testuint,
+      testuint2,
       testarray,
+      testarray2,
       testStruct,
       testRevert
     ] = result;
     expect(testuint).to.eq(BigNumber.from(10).pow(20));
+    expect(testuint2).to.deep.eq([
+      BigNumber.from(10).pow(18),
+      BigNumber.from(10).pow(19)
+    ])
     expect(testarray).to.deep.eq([
       BigNumber.from(10).pow(20),
       BigNumber.from(10).pow(18)
+    ]);
+    expect(testarray2).to.deep.eq([
+      BigNumber.from(10).pow(20)
     ]);
     expect(testStruct.a).to.eq(BigNumber.from(10).pow(18));
     expect(testStruct.b).to.deep.eq([50, 2]);
